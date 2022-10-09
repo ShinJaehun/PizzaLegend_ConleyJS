830ead79828a62a4b0af8f26dde008265101021a
ShinJaehun <gunsmoke.shin@gmail.com>
2022-10-09 14:55:06 +0900

    overworld maps

--- Overworld.js
+++ Overworld.js
@@ -3,14 +3,42 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
     }
+
+    startGameLoop(){
+        const step = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            // console.log("stepping");
+            // draw lower layer
+            this.map.drawLowerImage(this.ctx);
+
+            // draw game objects
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.x += 0.02;
+                object.sprite.draw(this.ctx);
+            });
+
+            // draw upper layer
+            this.map.drawUpperImage(this.ctx);
+
+            requestAnimationFrame(() => {
+                step()
+            });
+        }
+        step();
+    }
+
     init() {
+        this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+        this.startGameLoop();
+
         // console.log("Hello, from Overworld", this);
-        const image = new Image();
-        image.onload = () => {
-            this.ctx.drawImage(image, 0, 0); // 꼭 이렇게 그려야하는거야?
-        };
-        image.src = "images/maps/DemoLower.png";
+        // const image = new Image();
+        // image.onload = () => {
+        //     this.ctx.drawImage(image, 0, 0); // 꼭 이렇게 그려야하는거야?
+        // };
+        // image.src = "images/maps/DemoLower.png";
 
         // const x = 5;
         // const y = 6;
@@ -47,27 +75,20 @@ class Overworld {
         // };
         // hero.src = "images/characters/people/hero.png";
 
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        });
+        // const hero = new GameObject({
+        //     x: 5,
+        //     y: 6,
+        // });
 
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "images/characters/people/npc1.png"
-        });
+        // const npc1 = new GameObject({
+        //     x: 7,
+        //     y: 9,
+        //     src: "images/characters/people/npc1.png"
+        // });
 
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200) 
-        // 여기까지 해 놓고 계속 refresh 해도 object가 보이지 않는 이유를 알 수 없는 문제가 발생했음.
-        // 근데 이게 브라우저의 inspect 때문에 그려지는 위치가 벗어나 버린게 아닌가 싶다.
-        // inspect 끄고 다시 refresh하면 정상적으로 보임.
-        // 이런 비슷한 문제 해결방법 예전에 배웠던거 같은데... 어쨌든 계속 강좌를 따라가 보기로 함.
-
-        
-
+        // setTimeout(() => {
+        //     hero.sprite.draw(this.ctx);
+        //     npc1.sprite.draw(this.ctx);
+        // }, 200) 
     }
 }
